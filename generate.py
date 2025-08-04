9b7ab8e15e7987cfb817fb272fc1557edc05be17
Maximiliano Pinheiro de Oliveira <crmax80@gmail.com>
2025-08-04 11:58:30 -0300

    04AGO

---
+++ generate.py
@@ -0,0 +1,17 @@
+from random import choice
+import random
+
+# coin = choice(["cara", "coroa"])
+# print(coin)
+
+# number = random.randint(1, 10)
+# print(number)
+
+# cards = ["jack", "queen", "king"]
+# random.shuffle(cards)
+# print(cards)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
