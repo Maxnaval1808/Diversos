b93de3cd74790c09544bb4634d051fc4099d44b0
Maximiliano Pinheiro de Oliveira <crmax80@gmail.com>
2025-10-25 12:22:42 -0300

    25OUT25

---
+++ Tutorial.py
@@ -0,0 +1,34 @@
+o_mundo_é_plano = True
+if o_mundo_é_plano:
+    print("Cuidado para não cair da borda!")
+
+# este é o primeiro comentário
+spam = 1  # e este é o segundo comentário
+          # ... e agora um terceiro!
+texto = "# Este não é um comentário por estar entre aspas."
+
+2 + 2
+50 - 5*6
+(50 - 5*6) / 4
+8 / 5  # divisão sempre retorna um número de ponto flutuante
+
+17 / 3  # divisão clássica retorna um ponto flutuante
+17 // 3  # divisão pelo piso descarta a parte fracionária
+17 % 3  # o operador % retorna o resto da divisão
+5 * 3 + 2  # quociente do piso * divisor + restante
+
+5 ** 2  # 5 ao quadrado
+2 ** 7  # 2 à potência de 7
+
+largura = 20
+altura = 5 * 9
+largura * altura
+
+4 * 3.75 - 1
+
+taxa = 12.5 / 100
+preço = 100.50
+preço * taxa
+preço + _
+round(_, 2)
+
